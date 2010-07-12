c5dd28821cc046672c0db72e7582fa76db04706d
faridani <faridani@gmail.com>
2010-07-12 10:20:32 -0700

    :)

---
+++ dataTools/importMatlabData.py
@@ -0,0 +1,9 @@
+
+## This script imports Matlab data files ( .mat)
+# TODO: make it pythonic
+#       Blend it with the rest of the code
+
+
+import scipy.io
+
+mat = scipy.io.loadmat('opinion_space_live2.mat')
